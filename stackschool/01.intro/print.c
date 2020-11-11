84524416e0aa410285da795226a9bf6cf804623c
Md Shoriful Islam <shorifulislam433@gmail.com>
2020-11-11 07:25:29 +0600

    printing and mathmatics calculation in c just learn

---
+++ stackschool/01.intro/print.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("This is Md \tShoriful Islam.\n");
+    printf("I am starting to learn C programming.\n");
+    printf("My hobby to learn programming.\n");
+
+    printf("%d\n", 10+25);
+    printf("%d\n", 100-25);
+    printf("%d\n", 3*25);
+    printf("%d\n", 25/5);
+
+    return 0;
+}
